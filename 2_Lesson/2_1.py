d0f43fe585d5f47d33ca6362a96b11675bdb00d6
Vadamoff <Vadamoff@mail.ru>
2022-09-29 08:58:21 +0300

    Edited 2_1

--- 2_Lesson/2_1.py
+++ 2_Lesson/2_1.py
@@ -2,8 +2,17 @@
 # которая принимает на вход число N
 # и выдаёт последовательность из N членов.
 
+# 1 Вариант
+
+# N = int(input("Введите число:\n"))
+# prog = 1
+# for i in range(N):
+#     print(prog, end=' ')
+#     prog *= -3
+
+# 2 Вариант
+
 N = int(input("Введите число:\n"))
-prog = 1
-for i in range(1, N + 1):
-    print(prog, end=' ')
-    prog *= -3
+prog = -3
+for i in range(N):
+    print(prog ** i, end=' ')
