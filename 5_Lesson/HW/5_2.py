f484c3f0788bca838d63c22e61e91598dc2e9a86
Vadamoff <Vadamoff@mail.ru>
2022-10-13 19:52:58 +0300

    Edited HW 5_2

--- 5_Lesson/HW/5_2.py
+++ 5_Lesson/HW/5_2.py
@@ -2,41 +2,72 @@
 # реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
+from os import path
+from itertools import groupby, starmap
+
+def check(data):
+    return path.exists(data)
+
+# encode 1 вариант
+
+# def encode(data: str):
+#     encoding = ""
+#     prev_char = ''
+#     count = 1
+
+#     if not data:
+#         return "Файл пустой"
+
+#     else:
+#         for char in data:
+#             if char != prev_char:
+#                 if prev_char:
+#                     encoding += str(count) + prev_char
+#                 count = 1
+#                 prev_char = char
+#             else:
+#                 count += 1
+#         encoding += str(count) + prev_char
+#         return encoding
+
+# encode 2 вариант
+
 def encode(data: str):
-    encoding = ""
-    prev_char = ''
-    count = 1
+    chars = [str(len(list(g))) + k for k, g in groupby(data)]
+    return ''.join(chars)
 
-    if not data:
-        return "Файл пустой"
+# decode 1 вариант
 
-    else:
-        for char in data:
-            if char != prev_char:
-                if prev_char:
-                    encoding += str(count) + prev_char
-                count = 1
-                prev_char = char
-            else:
-                count += 1
-        encoding += str(count) + prev_char
-        return encoding
+# def decode(data: str):
+#     count = ""
+#     decode = ""
 
+#     for char in data:
+#         if char.isdigit():
+#             count += char
+#         else:
+#             decode += int(count) * char
+#             count = ""
+#     return decode
 
+# decode 2 вариант
 
 def decode(data: str):
+    pairs = []
     count = ""
-    decode = ""
 
-    for char in data:
-        if char.isdigit():
-            count += char
+    for i in data.strip():
+        if i.isdigit():
+            count += i
         else:
-            decode += int(count) * char
+            pairs.append([int(count), i])
             count = ""
-    return decode
+    return "".join(starmap(lambda x, y: x * y, pairs))
 
+# decode 3 вариант - не додумался... (
 
+# def decode(data: str):
+#     print([k for k, g in groupby(data) if k.isdigit()])
 
 def read(file: str):
     with open(file, "r", encoding="utf-8") as f:
@@ -46,13 +77,24 @@ def write(file: str, content: str):
     with open(file, "w", encoding="utf-8") as f:
         f.write(content)
 
-text_to_encode = input("Укажите файл с текстом для сжатия:\n")
-print(f"Исходный текст:\n{read(text_to_encode)}")
-encoded_text = encode(read(text_to_encode))
-print(f"Сжатый текст:\n{encoded_text}")
-comp_text = write(input("Укажите файл для сжатого текста:\n"), encoded_text)
+file_to_encode = input("Укажите файл с текстом для сжатия:\n")
+if check(file_to_encode):
+    print(f"Исходный текст:\n{read(file_to_encode)}")
+    encoded_text = encode(read(file_to_encode))
+    print(f"Сжатый текст:\n{encoded_text}")
+else:
+    print("Файла не существует или указан неверный путь")
 
-text_to_decode = input("Укажите файл с сжатым текстом:\n")
-print(f"Сжатый текст:\n{read(text_to_decode)}")
-decoded_text = decode(read(text_to_decode))
-print(f"Восстановленный текст:\n{decoded_text}")
+comp_file = input("Укажите файл для сжатого текста:\n")
+if check(comp_file):
+    comp_text = write(comp_file, encoded_text)
+else:
+    print("Файла не существует или указан неверный путь")
+
+file_to_decode = input("Укажите файл с сжатым текстом:\n")
+if check(file_to_decode):
+    print(f"Сжатый текст:\n{read(file_to_decode)}")
+    decoded_text = decode(read(file_to_decode))
+    print(f"Восстановленный текст:\n{decoded_text}")
+else:
+    print("Файла не существует или указан неверный путь")
